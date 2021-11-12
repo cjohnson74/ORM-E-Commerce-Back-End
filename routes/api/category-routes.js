d0df37630abc2cc2320e90268cd6afef724dc16f
Carson Johnson <carson74johnson@gmail.com>
2021-11-12 14:34:36 -0500

    fixed get all categories route as well as post a new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,23 +5,16 @@ const { Category, Product, ProductTag } = require("../../models");
 router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  try {
-    const categoryData = Category.findAll({
-      indlude: [
-        {
-          model: Product,
-          key: category_id,
-          as: category_products,
-        },
-      ],
-    });
-    // const category = categoryData.map((category) =>
-    //   category.get({ plain: true })
-    // );
-    res.status(200).json(categoryData);
-  } catch (err) {
+  Category.findAll(
+    {
+      indlude: Product
+    })
+    .then((category) => 
+      res.status(200).json(category))
+    .catch((err) => {
+    console.log(err);
     res.status(500).json(err);
-  }
+  });
 });
 
 router.get("/:id", (req, res) => {
@@ -46,12 +39,13 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
   // create a new category
-  try {
-    const categoryData = Category.create(req.body);
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+    Category.create(req.body)
+    .then((category) => 
+      res.status(200).json(category))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
