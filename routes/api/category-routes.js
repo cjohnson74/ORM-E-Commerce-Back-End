0d3f59ccafe248b19cb428ae47955d572692b5d9
Carson Johnson <carson74johnson@gmail.com>
2021-11-12 14:59:42 -0500

    fixed find category by id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,43 +5,42 @@ const { Category, Product, ProductTag } = require("../../models");
 router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  Category.findAll(
-    {
-      indlude: Product
-    })
-    .then((category) => 
-      res.status(200).json(category))
+  Category.findAll({
+    include: Product,
+  })
+    .then((category) => res.status(200).json(category))
     .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  try {
-    const categoryData = Category.findByPk(req.params.id, {
-      include: [{ model: Product, key: product_name, as: category_products }],
-      where: { category_id: req.params.id },
+  Category.findByPk(req.params.id, {
+    where: {
+      id: req.params.id,
+    },
+    include: Product, where: { category_id: req.params.id },
+  })
+    .then((category) => {
+      if (!category) {
+        res.status(404).json({ message: "No category found with this id!" });
+        return;
+      }
+      res.status(200).json(category);
+    })
+    .then((category) => res.status(200).json(category))
+    .catch((err) => {
+      res.status(500).json(err);
     });
-
-    if (!categoryData) {
-      res.status(404).json({ message: "No category found with this id!" });
-      return;
-    }
-
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
 router.post("/", (req, res) => {
   // create a new category
-    Category.create(req.body)
-    .then((category) => 
-      res.status(200).json(category))
+  Category.create(req.body)
+    .then((category) => res.status(200).json(category))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
