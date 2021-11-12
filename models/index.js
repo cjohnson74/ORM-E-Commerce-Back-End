7fde736c5e7d744e6e43d7dd89bfcb6638cad4a6
Carson Johnson <carson74johnson@gmail.com>
2021-11-12 10:49:27 -0500

    fixed index.js bug

--- models/index.js
+++ models/index.js
@@ -16,12 +16,12 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'tag_id',
+  foreignKey: 'product_id',
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: "product_id",
+  foreignKey: "tag_id",
 });
 
 module.exports = {
