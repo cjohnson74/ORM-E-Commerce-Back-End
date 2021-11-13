00c2991de72da52bb240ee5e9fcf960d3605f81b
Carson Johnson <carson74johnson@gmail.com>
2021-11-12 19:09:06 -0500

    added delete a tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,52 +41,46 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
   // create a new tag
-  try {
-    const tagData = Tag.create(req.body);
-    res.status(200).json(tagData);
-  } catch (err) {
+  Tag.create(req.body)
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => {
     res.status(400).json(err);
-  }
+  });
 });
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
-  try {
-    const tagData = Tag.update({
+  Tag.update(req.body, {
       where: {
         id: req.params.id,
       },
-    });
-
-    if (!tagData) {
-      res.status(404).json({ message: "No tag found with this id!" });
-      return;
-    }
-
-    res.status(200).json(tagData);
-  } catch (err) {
+    })
+    .then((tag) => {
+      if (!tag) {
+        res.status(404).json({ message: "No tag found with this id!" });
+        return;
+      }
+      res.status(200).json(tag);
+    })
+    .catch((err) => {
     res.status(500).json(err);
-  }
+  });
 });
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
-  try {
-    const tagData = Tag.destroy({
+  Tag.destroy({
       where: {
         id: req.params.id,
       },
-    });
-
-    if (!tagData) {
-      res.status(404).json({ message: "No tag found with this id!" });
-      return;
-    }
-
-    res.status(200).json(tagData);
-  } catch (err) {
+    })
+    .then((tag) => {
+      console.log(tag);
+      res.json(tag)
+    })
+    .catch((err) => {
     res.status(500).json(err);
-  }
+  });
 });
 
 module.exports = router;
